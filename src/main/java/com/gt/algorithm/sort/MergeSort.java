4ac6a0a7cfcc64a2f99208cd49d1ba338041bfe2
GTsung <xc_goo@163.com>
2022-01-04 19:28:22 +0800

    mergeSort

---
+++ src/main/java/com/gt/algorithm/sort/MergeSort.java
@@ -0,0 +1,53 @@
+package com.gt.algorithm.sort;
+
+import java.util.Arrays;
+
+/**
+ * 归并排序 O(nlogn)
+ *
+ * @author GTsung
+ * @date 2022/1/4
+ */
+public class MergeSort {
+
+    public static void main(String[] args) {
+        int[] arr = {12, 34, 11, 2, 7, 53, 1, 25, 25, 19};
+        mergeSort(arr);
+        Arrays.stream(arr).forEach(System.out::println);
+    }
+
+    private static void mergeSort(int[] arr) {
+        merge(arr, 0, arr.length - 1, new int[arr.length]);
+    }
+
+    private static void merge(int[] arr, int left, int right, int[] temp) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            merge(arr, left, mid, temp);
+            merge(arr, mid + 1, right, temp);
+            sort(arr, left, mid + 1, right, temp);
+        }
+    }
+
+    private static void sort(int[] arr, int leftPos, int rightPos, int rightEnd, int[] temp) {
+        int leftEnd = rightPos - 1;
+        int tempPos = leftPos;
+
+        int elementNum = rightEnd - leftPos + 1;
+
+        while (leftPos <= leftEnd && rightPos <= rightEnd) {
+            temp[tempPos++] = arr[leftPos] < arr[rightPos] ? arr[leftPos++] : arr[rightPos++];
+        }
+        while (leftPos <= leftEnd) {
+            temp[tempPos++] = arr[leftPos++];
+        }
+        while (rightPos <= rightEnd) {
+            temp[tempPos++] = arr[rightPos++];
+        }
+
+        for (int i = 0; i < elementNum; i++, rightEnd--) {
+            arr[rightEnd] = temp[rightEnd];
+        }
+
+    }
+}
