879081c7a6d3542e91cfd6f1b1128f7bb243c1b7
GTsung <xc_goo@163.com>
2022-01-09 15:54:32 +0800

    quick

--- src/main/java/com/gt/algorithm/sort/QuickSort.java
+++ src/main/java/com/gt/algorithm/sort/QuickSort.java
@@ -3,6 +3,9 @@ package com.gt.algorithm.sort;
 import java.util.Arrays;
 
 /**
+ * 快速排序 O(nlogn)
+ * 随机选取一个基准数，划分数组的小于等于大于区域
+ * 将小于区域左移并置换 最后将基准数放入到小于区域的前一位，返回基准数的下标
  * @author GTsung
  * @date 2022/1/5
  */
@@ -12,34 +15,35 @@ public class QuickSort {
         int[] arr = {12, 34, 11, 2, 7, 53, 1, 25, 25, 19};
         quickSort(arr, 0, arr.length - 1);
         Arrays.stream(arr).forEach(System.out::println);
+
     }
 
     private static void quickSort(int[] arr, int left, int right) {
-        Integer chosen = median3(arr, left, right);
-        int i = left, j = right - 1;
-        for (; ; ) {
-            while (arr[++i] < chosen) {
-            }
-            while (arr[--j] > chosen) {
-            }
-            if (i < j) {
-                swap(arr, i, j);
-            } else {
-                break;
-            }
+        if (left < right) {
+            // 随机选择一个下标作为基准(在分区方法中，这个置换到最右侧的元素被指定为基准数)
+            swap(arr, left + ((int) (Math.random() * (right - left + 1))), right);
+            // 分区，返回的数组个数为2
+            int p = partition(arr, left, right);
+            quickSort(arr, left, p - 1);
+            quickSort(arr, p + 1, right);
         }
-        swap(arr, i, right - 1);
-        quickSort(arr, left, i - 1);
-        quickSort(arr, i + 1, right);
     }
 
-    private static Integer median3(int[] arr, int left, int right) {
-        int center = (left + right) / 2;
-        if (arr[center] < arr[left]) swap(arr, left, center);
-        if (arr[right] < arr[left]) swap(arr, left, right);
-        if (arr[right] < arr[center]) swap(arr, center, right);
-        swap(arr, center, right - 1);
-        return arr[right - 1];
+    // 划分大于等于小于区域，返回等于区域下标
+    private static int partition(int[] arr, int left, int right) {
+        int pivot = arr[right];
+        // 小于基数的下标
+        int less = left - 1;
+        // 中间区域，如果中间区域的值小于pivot则小于区域的下标向前移动，当前下标的值与小于区域的值互换，
+        for (int j = left; j < right; j++) {
+            if (arr[j] < pivot) {
+                less++;
+                swap(arr, less, j);
+            }
+        }
+        // 最后将选中的数放到小于区域的下一位
+        swap(arr, less + 1, right);
+        return (less + 1);
     }
 
     private static void swap(int[] arr, int i, int j) {
