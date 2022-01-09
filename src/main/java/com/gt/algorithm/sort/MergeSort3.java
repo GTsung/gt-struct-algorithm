b34de8a9b1befc50e67541430a01388178c08fb4
GTsung <xc_goo@163.com>
2022-01-09 14:54:53 +0800

    逆序对

---
+++ src/main/java/com/gt/algorithm/sort/MergeSort3.java
@@ -0,0 +1,20 @@
+package com.gt.algorithm.sort;
+
+/**
+ * 逆序对
+ * @author GTsung
+ * @date 2022/1/9
+ */
+public class MergeSort3 {
+
+    /**
+     * 在一个数组中，如果左侧数字比右侧数字大，则这两个数称为逆序对，求所有逆序对数量
+     * @param args
+     */
+    public static void main(String[] args) {
+        // 逆序对----> 左侧大于右侧 (与求小和问题相反)
+    }
+
+
+
+}
