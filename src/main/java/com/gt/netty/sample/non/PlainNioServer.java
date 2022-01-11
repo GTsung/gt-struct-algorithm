d343758d10f892b543000da8e4e9d27eae8ecf0c
GTsung <xc_goo@163.com>
2022-01-11 14:20:48 +0800

    non netty

---
+++ src/main/java/com/gt/netty/sample/non/PlainNioServer.java
@@ -0,0 +1,64 @@
+package com.gt.netty.sample.non;
+
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * 不使用Netty的非阻塞NIO模式
+ *
+ * @author GTsung
+ * @date 2022/1/11
+ */
+public class PlainNioServer {
+
+    /**
+     * 非阻塞模式，适合并发场景
+     * @param port
+     * @throws Exception
+     */
+    public void server(int port) throws Exception {
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.configureBlocking(false);
+        ServerSocket serverSocket = serverSocketChannel.socket();
+        InetSocketAddress address = new InetSocketAddress(port);
+        serverSocket.bind(address);
+        Selector selector = Selector.open();
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+        final ByteBuffer msg = ByteBuffer.wrap("hi".getBytes());
+        for (;;) {
+            selector.select();
+            Set<SelectionKey> readyKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = readyKeys.iterator();
+            while (iterator.hasNext()) {
+                SelectionKey key = iterator.next();
+                iterator.remove();
+
+                if (key.isAcceptable()) {
+                    ServerSocketChannel server = (ServerSocketChannel) key.channel();
+                    SocketChannel client = server.accept();
+                    client.configureBlocking(false);
+                    client.register(selector,
+                            SelectionKey.OP_WRITE | SelectionKey.OP_READ, msg.duplicate());
+                    System.out.println("accepted from " + client);
+                }
+                if (key.isWritable()) {
+                    SocketChannel client = (SocketChannel) key.channel();
+                    ByteBuffer buffer = (ByteBuffer) key.attachment();
+                    while (buffer.hasRemaining()) {
+                        if (client.write(buffer) == 0) {
+                            break;
+                        }
+                    }
+                    client.close();
+                }
+            }
+        }
+    }
+}
