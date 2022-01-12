85cab80d10b4118aba6c8e1cf2b16812e8c3e9c3
GTsung <xc_goo@163.com>
2022-01-13 00:18:07 +0800

    client

---
+++ src/main/java/com/gt/netty/base/NettyClient.java
@@ -0,0 +1,35 @@
+package com.gt.netty.base;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author GTsung
+ * @date 2022/1/13
+ */
+public class NettyClient {
+
+    public static void main(String[] args) {
+        new Bootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                super.channelRead(ctx, msg);
+                            }
+                        });
+                    }
+                }).remoteAddress(new InetSocketAddress("localhost", 9000));
+    }
+}
