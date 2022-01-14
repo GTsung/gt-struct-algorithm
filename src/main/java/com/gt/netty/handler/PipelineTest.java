19e7031579c733e1fe24c413a419324d151db20d
GTsung <xc_goo@163.com>
2022-01-14 21:15:49 +0800

    handler

--- src/main/java/com/gt/netty/handler/PipelineTest.java
+++ src/main/java/com/gt/netty/handler/PipelineTest.java
@@ -1,12 +1,13 @@
 package com.gt.netty.handler;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.buffer.ByteBuf;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author GTsung
@@ -27,30 +28,35 @@ public class PipelineTest {
                         pipeline.addLast("h1", new ChannelInboundHandlerAdapter(){
                             @Override
                             public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                                System.out.println("-------" +((ByteBuf)msg).toString());
+                                System.out.println("-------handler1");
+                                // super.channelRead將消息msg傳遞給後面的入站handler
                                 super.channelRead(ctx, msg);
                             }
                         });
                         pipeline.addLast("h2", new ChannelInboundHandlerAdapter(){
                             @Override
                             public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                                log.info("收到信息{}", msg);
+                                System.out.println("-------handler2");
                                 // channel從最尾部開始找出站
-//                                pipeline.channel().writeAndFlush("as");
+//                                ctx.channel().writeAndFlush(ctx.alloc().buffer()
+//                                        .writeBytes("Server...".getBytes(StandardCharsets.UTF_8)));
                                 // ctx從這個入站前找出站handler，因此h3\h4不會收到
-                                ctx.writeAndFlush("as");
+                                ctx.writeAndFlush(ctx.alloc().buffer()
+                                        .writeBytes("Server...".getBytes(StandardCharsets.UTF_8)));
                                 super.channelRead(ctx, msg);
                             }
                         });
                         pipeline.addLast("h3", new ChannelOutboundHandlerAdapter() {
                             @Override
                             public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                                System.out.println("----handler3");
                                 super.write(ctx, msg, promise);
                             }
                         });
                         pipeline.addLast("h4", new ChannelOutboundHandlerAdapter() {
                             @Override
                             public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                                System.out.println("----handler4");
                                 super.write(ctx, msg, promise);
                             }
                         });
