f1f6fa6d1632779aa8556c249be88b11e7a782e3
GTsung <xc_goo@163.com>
2022-03-28 20:46:30 +0800

    threadLocal

--- src/main/java/com/gt/base/lang/ThreadTest.java
+++ src/main/java/com/gt/base/lang/ThreadTest.java
@@ -6,12 +6,25 @@ package com.gt.base.lang;
  */
 public class ThreadTest {
 
-    public static void main(String[] args) {
-        createThread();
+    private static ThreadLocal<String> threadLocal = new ThreadLocal<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(() -> {
+            threadLocal.set("01");
+            print("thread1");
+            System.out.println("after remove - " + threadLocal.get());
+        }).start();
+
+        new Thread(() -> {
+            threadLocal.set("02");
+            print("thread2");
+            System.out.println("after remove - " + threadLocal.get());
+        }).start();
     }
 
-    private static void createThread() {
-        Thread t = new Thread();
-        t.start();
+    private static void print(String str) {
+        System.out.println(str + " - " + threadLocal.get());
+        threadLocal.remove();
     }
+
 }
