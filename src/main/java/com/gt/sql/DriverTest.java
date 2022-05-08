fd7c7fcb7e48b1860f42758d86f60a66dde51db3
GTsung <xc_goo@163.com>
2022-05-08 21:25:59 +0800

    sql

---
+++ src/main/java/com/gt/sql/DriverTest.java
@@ -0,0 +1,14 @@
+package com.gt.sql;
+
+import java.sql.Driver;
+
+/**
+ * @author GTsung
+ * @date 2022/4/12
+ */
+public class DriverTest {
+
+    public static void main(String[] args) {
+        Driver driver = null;
+    }
+}
